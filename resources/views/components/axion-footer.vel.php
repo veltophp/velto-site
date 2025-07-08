1f3ae363318cc6d0da518800af782668422ec662
veltophp <dev@veltophp.com>
2025-07-08 17:35:19 +0800

    update

--- resources/views/components/axion-footer.vel.php
+++
@@ -1,6 +0,0 @@
-<div class="text-center mt-auto py-6">
-    <div class="font-light text-sm text-gray-500">
-        Running on PHP <span class="text-red-600">{{ phpversion() }}</span> | VeltoPHP V2.0<br>
-        Page generated in <span class="text-blue-600">{{ number_format((microtime(true) - VELTO_START) * 1000, 2) }}</span> ms
-    </div>
-</div>
