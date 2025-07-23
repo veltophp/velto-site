6a5232d9882bfcad2347db7970c01539a94d8c0f
veltophp <dev@veltophp.com>
2025-07-23 16:18:43 +0800

    update veltoadmin

---
+++ modules/VeltoAdmin/Api.php
@@ -0,0 +1,7 @@
+<?php
+
+use Velto\Core\Route\Route;
+use Modules\VeltoAdmin\VeltoAdminController;
+
+// Define API routes here
+// Route::get('/api/veltoadmin', [VeltoAdminController::class, 'veltoadmin']);
